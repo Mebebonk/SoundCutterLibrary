e1e3828041977935d78fa46b79cab5c2628a1e2f
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2023-10-14 23:58:39 +0500

    Add ProcessFile

--- SoundCutterLibrary/source/CutterApi.cs
+++ SoundCutterLibrary/source/CutterApi.cs
@@ -1,7 +1,21 @@
-﻿namespace SoundCutterLibrary
-{
-    public class CutterAPI
-    {
+﻿using NAudio.MediaFoundation;
+using NAudio.Wave;
+using System;
 
-    }
+namespace SoundCutterLibrary
+{
+	public class CutterAPI
+	{
+		public Task ProcessFile(string inputPath, string outputPath, Action<float> progressCallback, out Action callProgressCallback)
+		{
+			using WaveStream inputStream = new MediaFoundationReader(inputPath);
+			using Stream outputStream = File.OpenWrite(outputPath);
+			IProgress<AudioCutter> progress = new Progress<AudioCutter>((AudioCutter cutter) => progressCallback(cutter.State));
+			AudioCutter cutter = new AudioCutter(inputStream, new WaveFileWriter(outputStream, inputStream.WaveFormat));
+
+			callProgressCallback = () => progress.Report(cutter);
+
+			return Task.Run(cutter.Process);
+		}
+	}
 }
