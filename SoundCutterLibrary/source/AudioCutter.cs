81d80992fa3ee4c77b8fa8a81f938d477631203a
Arkadiy Zakharov <69598137+Mebebonk@users.noreply.github.com>
2023-10-14 21:48:03 +0500

    Seted up interface

--- SoundCutterLibrary/source/AudioCutter.cs
+++ SoundCutterLibrary/source/AudioCutter.cs
@@ -1,7 +1,19 @@
-﻿namespace SoundCutterLibrary
-{
-    public class AudioCutter
-    {
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    }
+namespace SoundCutterLibrary
+{
+	internal class AudioCutter
+	{
+
+		public AudioCutter(WaveStream audioInput, WaveStream audioOutput) 
+		{
+			
+		}
+
+	}
 }
